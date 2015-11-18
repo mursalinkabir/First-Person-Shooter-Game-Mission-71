df27b6994bd6125a655defb5c831c9d5947dd419
Mursalin Kabir <mail2mursalin@gmail.com>
2015-11-18 21:09:07 +0600

    EnemyAI2 ar intermediate obostha

--- EnemyAI2.js
+++ EnemyAI2.js
@@ -1,11 +1,13 @@
 var MoveSpeed: float=2;
 var Player: Transform;
 var number=3;
-var way1: Transform;
+var wayPoint: Transform;
+var wayPoints:Transform[];
 var MaxDist=10;
 var MinDist=20;
 var Health: float=100;
 var Range=30;
+var currentIndex;
 var DeadReplacement: GameObject;
 private var soldier: GameObject;
 function Start () {
@@ -15,6 +17,7 @@ function Start () {
 function Update () {
 	
     if(Vector3.Distance(transform.position,Player.position)<=Range){
+       //Chasing Player code
         transform.LookAt(Player);// find player in arena
 	if(Vector3.Distance(transform.position,Player.position)>=MinDist){
 	// if player is not closer to mindist run else stop
@@ -26,17 +29,20 @@ function Update () {
 	
 	}
     }else{
-	//soldier.animation.CrossFade("stand");
+	
         // if player goes far away the enemy will look at waypoint and go there and stand finally
-        transform.LookAt(way1);
-        if(Vector3.Distance(transform.position,way1.position)>=MinDist){
+        //currentIndex = Random.Range(0,wayPoints.Length);
+        transform.LookAt(wayPoints[0]);
+        if(Vector3.Distance(transform.position,wayPoints[0].position)>=MinDist){
         transform.position+= transform.forward*MoveSpeed*Time.deltaTime;
         soldier.animation.CrossFade("Walking");
         }
         
        else {
         soldier.animation.CrossFade("stand");
-            //soldier.animation.CrossFade("Walking");
+//            currentIndex = Random.Range(0,wayPoints.Length);
+//           transform.LookAt(wayPoints[currentIndex]);
+           
         }
     }
 	if(Vector3.Distance(transform.position,Player.position)<=MaxDist){
